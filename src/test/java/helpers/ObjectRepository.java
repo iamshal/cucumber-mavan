11a30d317ae9ba01d3f5300cb6049e2e5b88e21c
iamshal <34955157+iamshal@users.noreply.github.com>
2025-07-29 16:17:52 +0800

    Fix resource loading and driver path

--- src/test/java/helpers/ObjectRepository.java
+++ src/test/java/helpers/ObjectRepository.java
@@ -5,9 +5,10 @@ import java.io.InputStream;
 import java.util.Properties;
 public class ObjectRepository {
 	public static Properties OR = new Properties();
-	public static Properties ObjectRepo(String filepath) throws IOException {
-		InputStream in = new FileInputStream(filepath);
-		OR.load(in);
-		return OR;
-	}
+        public static Properties ObjectRepo(String filepath) throws IOException {
+                try (InputStream in = new FileInputStream(filepath)) {
+                        OR.load(in);
+                }
+                return OR;
+        }
 }
